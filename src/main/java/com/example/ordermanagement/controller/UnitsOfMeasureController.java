d0a5f239788e517b418122344da2140a2fe00614
Carla1104 <falubcarla98@gmail.com>
2025-11-04 01:18:23 +0200

    final controllere

--- src/main/java/com/example/ordermanagement/controller/UnitsOfMeasureController.java
+++ src/main/java/com/example/ordermanagement/controller/UnitsOfMeasureController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/unitsofmeasure-view")
+@RequestMapping("/unitsofmeasure")
 public class UnitsOfMeasureController extends GenericController<UnitsOfMeasure> {
 
     public UnitsOfMeasureController(UnitsOfMeasureService service) {
