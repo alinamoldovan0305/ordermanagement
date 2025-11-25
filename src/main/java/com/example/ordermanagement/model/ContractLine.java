346d0044cb0371ac3f992a021fa5cd390bacfbba
Carla1104 <falubcarla98@gmail.com>
2025-11-25 13:17:05 +0200

    details contracttype

--- src/main/java/com/example/ordermanagement/model/ContractLine.java
+++ src/main/java/com/example/ordermanagement/model/ContractLine.java
@@ -1,26 +1,88 @@
+//package com.example.ordermanagement.model;
+//
+//public class ContractLine {
+//
+//    private String id;
+//    private String itemId;   // <-- în loc de SellableItem item
+//    private String unitId;   // <-- în loc de UnitsOfMeasure unit
+//    private double quantity;
+//
+//    public ContractLine() {}
+//
+//    public ContractLine(String id, String itemId, String unitId, double quantity) {
+//        this.id = id;
+//        this.itemId = itemId;
+//        this.unitId = unitId;
+//        this.quantity = quantity;
+//    }
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getItemId() {
+//        return itemId;
+//    }
+//
+//    public void setItemId(String itemId) {
+//        this.itemId = itemId;
+//    }
+//
+//    public String getUnitId() {
+//        return unitId;
+//    }
+//
+//    public void setUnitId(String unitId) {
+//        this.unitId = unitId;
+//    }
+//
+//    public double getQuantity() {
+//        return quantity;
+//    }
+//
+//    public void setQuantity(double quantity) {
+//        this.quantity = quantity;
+//    }
+//}
 package com.example.ordermanagement.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contract_lines")
 public class ContractLine {
 
-    private String id;
-    private String itemId;   // <-- în loc de SellableItem item
-    private String unitId;   // <-- în loc de UnitsOfMeasure unit
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // ID auto-generat în loc de String
+
+    @Column(name = "item_id", nullable = false)
+    private String itemId;
+
+    @Column(name = "unit_id", nullable = false)
+    private String unitId;
+
+    @Column(nullable = false)
     private double quantity;
 
+    // Constructor fără argumente (necesar pentru JPA)
     public ContractLine() {}
 
-    public ContractLine(String id, String itemId, String unitId, double quantity) {
-        this.id = id;
+    public ContractLine(String itemId, String unitId, double quantity) {
         this.itemId = itemId;
         this.unitId = unitId;
         this.quantity = quantity;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -46,5 +108,15 @@ public class ContractLine {
 
     public void setQuantity(double quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "ContractLine{" +
+                "id=" + id +
+                ", itemId='" + itemId + '\'' +
+                ", unitId='" + unitId + '\'' +
+                ", quantity=" + quantity +
+                '}';
     }
 }
--- src/main/java/com/example/ordermanagement/model/ContractType.java
+++ src/main/java/com/example/ordermanagement/model/ContractType.java
@@ -1,23 +1,82 @@
+//package com.example.ordermanagement.model;
+//
+//public class ContractType {
+//    private String id;
+//    private String name;
+//    private String type;
+//
+//    public ContractType(String id, String name, String type) {
+//        this.id = id;
+//        this.name = name;
+//        this.type = type;
+//    }
+//
+//    public ContractType () {}
+//
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "ContractType{" +
+//                "id='" + id + '\'' +
+//                ", name='" + name + '\'' +
+//                ", type='" + type + '\'' +
+//                '}';
+//    }
+//
+//}
 package com.example.ordermanagement.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "contract_types")
 public class ContractType {
-    private String id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   // înlocuit String -> Long pentru baza de date
+
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, length = 50)
     private String type;
 
-    public ContractType(String id, String name, String type) {
-        this.id = id;
+    public ContractType() {}
+
+    public ContractType(String name, String type) {
         this.name = name;
         this.type = type;
     }
 
-    public ContractType () {}
-
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -40,10 +99,9 @@ public class ContractType {
     @Override
     public String toString() {
         return "ContractType{" +
-                "id='" + id + '\'' +
+                "id=" + id +
                 ", name='" + name + '\'' +
                 ", type='" + type + '\'' +
                 '}';
     }
-
 }
